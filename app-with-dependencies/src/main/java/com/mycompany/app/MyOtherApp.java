376a11687eeb327372532abe19d7695ea822f9f4
krenMaksim <krenmaba@gmail.com>
2018-01-01 12:10:09 +0300

    futher learning maven

--- app-with-dependencies/src/main/java/com/mycompany/app/MyOtherApp.java
+++ app-with-dependencies/src/main/java/com/mycompany/app/MyOtherApp.java
@@ -4,9 +4,12 @@ package com.mycompany.app;
  * Hello world!
  *
  */
-public class App {
+public class MyOtherApp {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		ForMyOtherApp.main(null);
+
+		ForMyOtherApp app = new ForMyOtherApp();
+		app.testMethod();
 	}
 }
